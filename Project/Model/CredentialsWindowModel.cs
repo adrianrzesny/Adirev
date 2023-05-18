e6228c209feaf8bb18e32028a540b2ff9212ce31
adrianrzesny <adrian.rzesny@gmail.com>
2023-05-18 20:35:00 +0200

    Added credentials window

---
+++ Project/Model/CredentialsWindowModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adirev.Model
+{
+    class CredentialsWindowModel
+    {
+        #region Variables
+        private string title = String.Empty;
+        private string login = String.Empty;
+        private string password = String.Empty;
+        #endregion
+
+        #region Properties
+        public string Title 
+        {
+            get => title; 
+            set => title = value; 
+        }
+        public string Login
+        {
+            get => login;
+            set => login = value;
+        }
+        public string Password
+        {
+            get => password; 
+            set => password = value;
+        }
+        #endregion
+
+        #region Construcotr
+        public CredentialsWindowModel()
+        {
+            Title = "Login Window";
+        }
+        #endregion
+    }
+}
---
+++ Project/ViewModel/CredentialsWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Adirev.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adirev.ViewModel
+{
+    class CredentialsWindowViewModel : NotifyPropertyChanged
+    {
+        #region Objects
+        private CredentialsWindowModel model = new CredentialsWindowModel();
+        #endregion
+
+        #region Properties
+
+        public string Title 
+        { 
+            get => model.Title; 
+            set
+            {
+                model.Title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+        public string Login
+        {
+            get => model.Login;
+            set
+            {
+                model.Login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+        public string Password
+        {
+            get => model.Password;
+            set
+            {
+                model.Password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        #endregion
+    }
+}
